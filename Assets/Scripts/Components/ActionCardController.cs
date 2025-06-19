e03ccdcbef4e9105ddd15099d9115c923b96e180
AndeeeeeZ <zhuandeee@gmail.com>
2025-06-18 20:48:48 -0400

    Better Action Card System
    Now all the card added is instantiated, and there's two button that can add two different type of cards based on scriptable objects

--- Assets/Scripts/Components/ActionCardController.cs
+++ Assets/Scripts/Components/ActionCardController.cs
@@ -10,7 +10,6 @@ public class ActionCardController : MonoBehaviour
     [SerializeField]
     private bool debugging;
 
-    #region References
     [SerializeField]
     private EventSystem eventSystem;
 
@@ -18,25 +17,27 @@ public class ActionCardController : MonoBehaviour
     private GraphicRaycaster raycaster;
 
     [SerializeField]
-    private List<GameObject> actionCardDisplays;
+    private GameObject actionCardUIPrefab;
 
-    [SerializeField] 
-    private ActionCard[] actionCards;
-
-    #endregion
+    [SerializeField]
+    private ActionCard[] actionCardScriptableObjects; 
 
     [SerializeField]
     private float gapBetweenCards, selectYOffset, cardMoveUpSpeed, cardMoveDownSpeed;
 
     [HideInInspector]
-    public int maxCardLimit, currentCardAmount;
+    public int currentCardAmount;
+
+    public int maxCardAmount; 
 
     public static ActionCardController Instance { get; private set; }
 
     private PointerEventData pointerEventData;
 
-    private float[] targetXLocation; 
-    private float[] targetYLocation;
+    private List<GameObject> actionCardDisplays;
+    private List<ActionCard> actionCards;
+    private List<float> targetXLocation; 
+    private List<float> targetYLocation;
 
     private int draggedCardIndex; 
 
@@ -49,15 +50,45 @@ public class ActionCardController : MonoBehaviour
         }
         Instance = this;
 
-        maxCardLimit = actionCardDisplays.Count;
-        currentCardAmount = actionCards.Length; 
-        targetXLocation = new float[maxCardLimit];
-        targetYLocation = new float[maxCardLimit];
+        targetXLocation = new List<float>();
+        targetYLocation = new List<float>();
+        actionCardDisplays = new List<GameObject>(); 
+        actionCards = new List<ActionCard>();
+
+        currentCardAmount = 0; 
+
+        InitializeActionCardList(3); 
+
         draggedCardIndex = -1; 
 
         CalculateTargetLocation();
-        UpdateDisplaySprite();
+    }
+
+    // Initialize with n action cards
+    private void InitializeActionCardList(int n)
+    {
+        for (int i = 0; i < n; i++)
+        {
+            AddCard(i, 0); 
+        }
+    }
+
+    public void AddCard(int cardObjectIndex)
+    {
+        AddCard(actionCardDisplays.Count, cardObjectIndex);
+    }
+
+    private void AddCard(int index, int cardObjectIndex)
+    {
+    // TODO: change this to the deck location
+        targetXLocation.Add(0f);
+        targetYLocation.Add(0f);
+        actionCardDisplays.Add(Instantiate(actionCardUIPrefab, this.transform));
+        actionCards.Add(actionCardScriptableObjects[cardObjectIndex]);
+        currentCardAmount++;
+        UpdateDisplaySprite(index); 
         
+        Debug.Log("An action card is instantiated");
     }
 
     private void Update()
@@ -152,17 +183,18 @@ public class ActionCardController : MonoBehaviour
         }
     }
 
+    // TODO: Change this to add a default card instead of showing one more existed card
     // Change current card amount by n
-    public void ChangeCurrentCardAmountBy(int n)
-    {
-        currentCardAmount += n; 
-        if (currentCardAmount <= 0 ||  currentCardAmount > maxCardLimit)
-        {
-            Debug.LogWarning("Card amount exceeds card amount limit"); 
-        }
-        currentCardAmount = Mathf.Clamp(currentCardAmount, 0, maxCardLimit);
-        CalculateTargetLocation(); 
-    }
+    //public void ChangeCurrentCardAmountBy(int n)
+    //{
+    //    currentCardAmount += n; 
+    //    if (currentCardAmount <= 0 ||  currentCardAmount > maxCardLimit)
+    //    {
+    //        Debug.LogWarning("Card amount exceeds card amount limit"); 
+    //    }
+    //    currentCardAmount = Mathf.Clamp(currentCardAmount, 0, maxCardLimit);
+    //    CalculateTargetLocation(); 
+    //}
 
     // Calculates the x position the cards need to move to 
     // In order to maintain the gap while centering the group of cards
@@ -171,38 +203,38 @@ public class ActionCardController : MonoBehaviour
         if (currentCardAmount == 0)
             return; 
 
-        UpdateDisplayActivity(); 
+        //UpdateDisplayActivity(); 
+
         float totalDistance = (currentCardAmount - 1) * gapBetweenCards;
         targetXLocation[0] = -(totalDistance / 2f); 
+
         for (int i = 1; i < currentCardAmount; i++)
         {
             targetXLocation[i] = targetXLocation[i-1] + gapBetweenCards;    
         }
 
-        // New cards spawns in the location of the right most card
-        for (int i = currentCardAmount; i < maxCardLimit; i++)
-        {
-            targetXLocation[i] = targetXLocation[currentCardAmount - 1]; 
-        }
+        //// New cards spawns in the location of the right most card
+        //for (int i = currentCardAmount; i < currentCardAmount; i++)
+        //{
+        //    targetXLocation[i] = targetXLocation[currentCardAmount - 1]; 
+        //}
     }
 
-    // Update all the current cards UI as active, vice versa
-    private void UpdateDisplayActivity()
+    //// Update all the current cards UI as active, vice versa
+    //private void UpdateDisplayActivity()
+    //{
+    //    for (int i = 0; i < currentCardAmount; i++)
+    //    {
+    //        if (i >= currentCardAmount)
+    //            actionCardDisplays[i].SetActive(false);
+    //        else
+    //            actionCardDisplays[i].SetActive(true);
+    //    }
+    //}
+    private void UpdateDisplaySprite(int index)
     {
-        for (int i = 0; i < maxCardLimit; i++)
-        {
-            if (i >= currentCardAmount)
-                actionCardDisplays[i].SetActive(false);
-            else
-                actionCardDisplays[i].SetActive(true);
-        }
-    }
-    private void UpdateDisplaySprite()
-    {
-        for (int i = 0; i < actionCards.Length; i++)
-        {
-            actionCardDisplays[i].GetComponent<Image>().sprite = actionCards[i].sprite;
-        }
+        actionCardDisplays[index].GetComponent<Image>().sprite = actionCards[index].sprite;
+        
     }
 
     public int GetDraggedCardIndex()
